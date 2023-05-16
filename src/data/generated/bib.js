ee23049b0f4705f1d268fb0f521d70f324930e55
annaseq <zhouxuan1201@126.com>
2023-05-16 22:32:53 +0100

    SurVis

---
+++ src/data/generated/bib.js
@@ -0,0 +1,114 @@
+﻿const generatedBibEntries = {
+    "10.1145/3495018.3501199": {
+        "address": "New York, NY, USA",
+        "author": "Ji, Shengxuan and Zeng, Yihe",
+        "booktitle": "2021 3rd International Conference on Artificial Intelligence and Advanced Manufacture",
+        "doi": "10.1145/3495018.3501199",
+        "isbn": "9781450385046",
+        "location": "Manchester, United Kingdom",
+        "numpages": "7",
+        "pages": "2874\u20132880",
+        "publisher": "Association for Computing Machinery",
+        "series": "AIAM2021",
+        "title": "Handwritten Character Recognition Based on CNN",
+        "type": "inproceedings",
+        "url": "https://doi.org/10.1145/3495018.3501199",
+        "year": "2022"
+    },
+    "1677974": {
+        "author": "Yang Liu and Shriberg, E. and Stolcke, A. and Hillard, D. and Ostendorf, M. and Harper, M.",
+        "doi": "10.1109/TASL.2006.878255",
+        "journal": "IEEE Transactions on Audio, Speech, and Language Processing",
+        "number": "5",
+        "pages": "1526-1540",
+        "title": "Enriching speech recognition with automatic detection of sentence boundaries and disfluencies",
+        "type": "article",
+        "volume": "14",
+        "year": "2006"
+    },
+    "4156191": {
+        "author": "De Wachter, Mathias and Matton, Mike and Demuynck, Kris and Wambacq, Patrick and Cools, Ronald and Van Compernolle, Dirk",
+        "doi": "10.1109/TASL.2007.894524",
+        "journal": "IEEE Transactions on Audio, Speech, and Language Processing",
+        "number": "4",
+        "pages": "1377-1390",
+        "title": "Template-Based Continuous Speech Recognition",
+        "type": "article",
+        "volume": "15",
+        "year": "2007"
+    },
+    "5166785": {
+        "author": "Kumar, T. Lalith and Kumar, T. Kishore and Rajan, K. Soundar",
+        "booktitle": "2009 International Conference on Signal Processing Systems",
+        "doi": "10.1109/ICSPS.2009.51",
+        "number": "",
+        "pages": "248-252",
+        "title": "Speech Recognition Using Neural Networks",
+        "type": "inproceedings",
+        "volume": "",
+        "year": "2009"
+    },
+    "6296526": {
+        "author": "Hinton, Geoffrey and Deng, Li and Yu, Dong and Dahl, George E. and Mohamed, Abdel-rahman and Jaitly, Navdeep and Senior, Andrew and Vanhoucke, Vincent and Nguyen, Patrick and Sainath, Tara N. and Kingsbury, Brian",
+        "doi": "10.1109/MSP.2012.2205597",
+        "journal": "IEEE Signal Processing Magazine",
+        "number": "6",
+        "pages": "82-97",
+        "title": "Deep Neural Networks for Acoustic Modeling in Speech Recognition: The Shared Views of Four Research Groups",
+        "type": "article",
+        "volume": "29",
+        "year": "2012"
+    },
+    "6880632": {
+        "author": "Adamski, Michal and Von Solms, Basie",
+        "booktitle": "2014 IST-Africa Conference Proceedings",
+        "doi": "10.1109/ISTAFRICA.2014.6880632",
+        "number": "",
+        "pages": "1-8",
+        "title": "An open speaker recognition enabled identification and authentication system",
+        "type": "inproceedings",
+        "volume": "",
+        "year": "2014"
+    },
+    "article": {
+        "author": "Gevaert, Wouter and Tsenov, Georgi and Mladenov, Valeri",
+        "doi": "10.2298/JAC1001001G",
+        "journal": "Journal of Automatic Control",
+        "month": "01",
+        "pages": "",
+        "title": "Neural networks used for speech recognition",
+        "type": "article",
+        "volume": "20",
+        "year": "2010"
+    },
+    "book": {
+        "author": "Wang, Shuyan",
+        "doi": "10.1515/jisys-2022-0236",
+        "journal": "Journal of Intelligent Systems",
+        "month": "02",
+        "pages": "",
+        "title": "Recognition of English speech \u2013 using a deep learning algorithm",
+        "type": "article",
+        "volume": "32",
+        "year": "2023"
+    },
+    "inproceedings": {
+        "author": "Huang, Xuedong",
+        "doi": "10.3115/112405.112458",
+        "month": "01",
+        "pages": "",
+        "title": "A Study on Speaker-Adaptive Speech Recognition.",
+        "type": "inproceedings",
+        "year": "1991"
+    },
+    "pour2009AnAM": {
+        "author": "Meysam Mohamad pour and Fardad Farokhi",
+        "doi": "10.5281/zenodo.1059445",
+        "journal": "International Journal of Electrical and Computer Engineering",
+        "pages": "115-120",
+        "title": "An Advanced Method for Speech Recognition",
+        "type": "article",
+        "volume": "3",
+        "year": "2009"
+    }
+};
